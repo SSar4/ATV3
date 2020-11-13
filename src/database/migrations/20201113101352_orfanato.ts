55179c8db078a5562308a82d99ee26eb2c606389
LuzEsperanza <luzdeesperanzaapolo@gmail.com>
2020-11-13 11:11:23 -0300

    controle orfanato

--- src/database/migrations/20201113101352_orfanato.ts
+++ src/database/migrations/20201113101352_orfanato.ts
@@ -2,9 +2,21 @@ import * as Knex from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('orfanto',function(table){
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.string('sobre').notNullable();
+        table.string('telefone').notNullable();
+        table.string('intrucoes').notNullable();
+        table.string('h_visita').notNullable();
+        table.boolean('fim_semana').notNullable()
+    });
+
 }
 
 
-export async function down(knex: Knex): Promise<void> {
+export async function down(knex: Knex): Promise<void> { 
+    knex.schema.dropTable('orfanato');
+
 }
 
