13ad60824a68c8d60e16ac13f4b50095766029e0
josé <joseifpb2015@gmail.com>
2020-11-19 00:21:37 -0300

    add images

--- src/controller/orfanatoController.ts
+++ src/controller/orfanatoController.ts
@@ -18,24 +18,32 @@ class orfanatoController {
  }
  async create(request: Request, response:Response){
  
-  const {nome,sobre,telefone,intrucoes,h_visita,fim_semana,longitude,latitude} = request.body
+  const {nome, sobre, telefone, intrucoes,  h_visita, fim_semana, longitude, latitude } = request.body
 
-  const project = {
-    nome,sobre,telefone,intrucoes,h_visita,fim_semana,longitude,latitude
-  }
+  console.log('dados',nome)
   //inicia uma transação
   const trx = await connection.transaction()
-  const docExists = await trx('orfanato').where({'longitude': longitude, 'latitude': latitude}).first()
+ 
 
-  if(docExists){
-      await trx.rollback()
-      return response.json({error:'error user already exist'})
+  const orfanato_insert = await trx('orfanato').insert({
+    nome,
+    sobre,
+    telefone, 
+    fim_semana,
+    intrucoes, 
+    h_visita,
+     longitude,
+     latitude 
+    } 
+
+  )
+  if(orfanato_insert){
+    await trx.commit()
+  return response.status(200).json({sucesso:'Orfanato salvo com sucesso'});
   }
-
-  const orfanato_insert = await trx('orfanato').insert(project)
+  return response.status(400).json({error:'erro ao salvar'});
   //se chegou aki e pq deu tudo certo e salva no banco
-  await trx.commit()
-  return response.json(docExists)
+  
 
 }
 
